85a7cf4c68abfb00d8c928d6b5b6f2fd7116bfa5
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2025-01-22 20:05:01 -0300

    show all heros in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,12 @@ export default function Home() {
               />
             </div>
             <div className="relative">
-              <div className="w-full h-[calc(100vh-28rem)] sm:h-[calc(100vh-27rem)] lg:h-[calc(100vh-10rem)]  relative z-0">
+              <div className="min-w-full min-h-[60vh] sm:min-h-[80vh] lg:min-h-[90vh] h-auto relative z-0">
                 <Image
                   src="/material/poster-14-gradiente.png"
                   alt="Intrépidos"
                   layout="fill"
-                  className="object-contain object-top z-0"
+                  className="object-cover object-center z-0"
                 />
               </div>
               <p className="text-white text-justify p-6 lg:p-8 font-semibold text-xl -mt-28 z-10 relative">
