5a248a012f220457563c3cb751d550ae67c31caf
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2025-02-06 18:53:45 -0300

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         </section>
 
         <section className="my-10 flex overflow-hidden flex-wrap justify-around gap-8 items-center w-full">
-          <div className="overflow-hidden relative rounded-full w-80 h-80 flex justify-center items-center shadow-lg">
+          {/* <div className="overflow-hidden relative rounded-full w-80 h-80 flex justify-center items-center shadow-lg">
             <Image
               src="/material/intrepidos.gif"
               className="object-cover"
@@ -40,7 +40,7 @@ export default function Home() {
               quality={1}
               layout="fill"
             />
-          </div>
+          </div> */}
 
           <HomeHighlights />
         </section>
