78d01affbfd9f927b850293264702749149c3b36
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2025-02-24 20:52:35 -0300

    feat: add e-book

--- src/app/produto/products/livro/livro-side.tsx
+++ src/app/produto/products/livro/livro-side.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { ShoppingBag } from "lucide-react";
+import { BookOpen, ShoppingBag } from "lucide-react";
 import React from "react";
 
 const LivroSide = () => {
@@ -12,9 +12,9 @@ const LivroSide = () => {
 
       <div className="relative mt-4 h-full flex flex-col justify-between">
         <div className="flex flex-col gap-2">
-          <div>
+          <div className="justify-center flex">
             <Button
-              className="bg-yellow-500 font-bold text-black hover:bg-yellow-400"
+              className="bg-yellow-500 font-bold text-black hover:bg-yellow-400 w-full"
               onClick={() =>
                 window.open(
                   "https://produto.mercadolivre.com.br/MLB-5283951856-livro-autor-eduardo-cagnotto-titulo-do-livro-intrepidos-livro-de-aventura-e-ficco-_JM",
@@ -25,8 +25,28 @@ const LivroSide = () => {
               Comprar Pelo Mercado Livre <ShoppingBag />
             </Button>
           </div>
+          <div className=" w-full h-px bg-zinc-200 my-6" />
+
+          <div className="justify-center flex flex-col gap-2">
+            <p className="text-center">
+              Compre na versão digital e economize 50%
+            </p>
+            <Button
+              className="bg-green-500 font-bold text-black hover:bg-green-400"
+              onClick={() =>
+                window.open(
+                  "https://ebookbox.com.br/app/escritor.php?id=252",
+                  "_blank"
+                )
+              }
+            >
+              Comprar E-BOOK <BookOpen />
+            </Button>
+          </div>
+          <div className=" w-full h-px bg-zinc-200 my-6" />
+
           <div className="relative flex flex-col gap-3 w-full">
-            <h2 className="text-lg font-semibold mt-10">Descrição:</h2>
+            <h2 className="text-lg font-semibold mt-5">Descrição:</h2>
             <p>
               E se a terra estivesse a beira da destruição, ameaçada por um
               inimigo implacável? Apenas um grupo de heróis intrépidos, cada um
